f427eaa53683c7c0ab723e2c47cdb44d1ef47c89
Radiant-F <radiantfadilah0@gmail.com>
2023-09-14 15:55:30 +0700

    import counter

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,5 +2,6 @@ import Home from './Home';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import SplashScreen from './SplashScreen';
+import Counter from './Counter';
 
-export {Home, SignIn, SignUp, SplashScreen};
+export {Home, SignIn, SignUp, SplashScreen, Counter};
