d37a15bce66fc3b90330d022628c4e3e8d7c5afb
Radiant-F <radiantfadilah0@gmail.com>
2023-09-14 15:51:16 +0700

    configure store & provider

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Navigator from './src/routes/Navigator';
+import {Provider} from 'react-redux';
+import {store} from './src/redux/store';
 
 export default function App() {
-  return <Navigator />;
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
 }
