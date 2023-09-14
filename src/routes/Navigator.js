689697a4c69b7b9f9c774fbb5263d2a47ba55712
Radiant-F <radiantfadilah0@gmail.com>
2023-09-14 15:53:07 +0700

    add counter screen

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home, SignIn, SignUp, SplashScreen} from '../screens';
+import {Counter, Home, SignIn, SignUp, SplashScreen} from '../screens';
 import RNBootSplash from 'react-native-bootsplash';
 
 const Stack = createNativeStackNavigator();
@@ -14,11 +14,13 @@ export default function Navigator() {
           headerShown: false,
           statusBarColor: 'transparent',
           statusBarTranslucent: true,
-        }}>
+        }}
+        initialRouteName="Counter">
         <Stack.Screen component={SplashScreen} name="SplashScreen" />
         <Stack.Screen component={SignIn} name="SignIn" />
         <Stack.Screen component={SignUp} name="SignUp" />
         <Stack.Screen component={Home} name="Home" />
+        <Stack.Screen component={Counter} name="Counter" />
       </Stack.Navigator>
     </NavigationContainer>
   );
