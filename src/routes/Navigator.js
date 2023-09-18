69869f85bb132fd2aed7941b4686ba4c6b1b9a8c
Radiant-F <radiantfadilah0@gmail.com>
2023-09-18 15:03:28 +0700

    update initial route name

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Counter, Home, SignIn, SignUp, SplashScreen} from '../screens';
+import {Home, SignIn, SignUp, SplashScreen, Counter} from '../screens';
 import RNBootSplash from 'react-native-bootsplash';
 
 const Stack = createNativeStackNavigator();
@@ -15,7 +15,7 @@ export default function Navigator() {
           statusBarColor: 'transparent',
           statusBarTranslucent: true,
         }}
-        initialRouteName="Counter">
+        initialRouteName="SplashScreen">
         <Stack.Screen component={SplashScreen} name="SplashScreen" />
         <Stack.Screen component={SignIn} name="SignIn" />
         <Stack.Screen component={SignUp} name="SignUp" />
