8b005c9cabaad9a9602c5491d67192219bf874a3
Radiant-F <radiantfadilah0@gmail.com>
2023-09-18 15:03:36 +0700

    update demo

--- src/screens/Counter.js
+++ src/screens/Counter.js
@@ -1,32 +1,29 @@
-import {StyleSheet, Text, View, Button} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
+import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {
-  incrementCount,
-  decrementCount,
-  addCountBy,
-  minusCountBy,
+  decrement,
+  decrementBy,
+  increment,
+  incrementBy,
 } from '../redux/slice/counterSlice';
 
 export default function Counter() {
   const dispatch = useDispatch();
-  const {age, count} = useSelector(state => state.counter);
-
+  const {count} = useSelector(state => state.counter);
   return (
     <View>
-      <Text style={{fontSize: 50}}>{count}</Text>
-      <Button title="increment" onPress={() => dispatch(incrementCount())} />
-      <Button title="decrement" onPress={() => dispatch(decrementCount())} />
+      <Text style={{fontSize: 50, textAlign: 'center'}}>{count}</Text>
+      <Button title="tambah" onPress={() => dispatch(increment())} />
+      <Button title="kurang" onPress={() => dispatch(decrement())} />
       <Button
-        title="tambah dengan jumlah..."
-        onPress={() => dispatch(addCountBy(15))}
+        title="tambah dengan..."
+        onPress={() => dispatch(incrementBy(30))}
       />
       <Button
-        title="kurangi dengan jumlah..."
-        onPress={() => dispatch(minusCountBy(35))}
+        title="kurangi dengan..."
+        onPress={() => dispatch(decrementBy(50))}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
