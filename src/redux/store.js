7b34c00d37edce749c877a9f7e76c574a3562b1c
Radiant-F <radiantfadilah0@gmail.com>
2023-09-18 15:01:47 +0700

    add user reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterSlice from './slice/counterSlice';
+import userSlice from './slice/userSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
+    user: userSlice,
   },
 });
