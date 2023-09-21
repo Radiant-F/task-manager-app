df30a0fe3661149e17c84f8e9c1d4ed5eaa53d1a
Radiant-F <radiantfadilah0@gmail.com>
2023-09-21 15:27:45 +0700

    update reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterSlice from './slice/counterSlice';
 import userSlice from './slice/userSlice';
+import signInSlice from './slice/signInSlice';
+import signUpSlice from './slice/signUpSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice,
     user: userSlice,
+    sign_in: signInSlice,
+    sign_up: signUpSlice,
   },
 });
